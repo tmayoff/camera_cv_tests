0c745659df2cc98642b6fe4e2a2970c9e2f6c36e
Tyler Mayoff <tyler@tylermayoff.com>
2021-06-18 17:36:21 -0400

    Simple depthai implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,43 @@
+#include <depthai/depthai.hpp>
 #include <iostream>
 
 #define PROJECT_NAME "camera_cv_tests"
 
-int main(int argc, char **argv) {
-    if(argc != 1) {
-        std::cout << argv[0] <<  "takes no arguments.\n";
-        return 1;
-    }
-    std::cout << "This is project " << PROJECT_NAME << ".\n";
-    return 0;
+int main() {
+  int height = 1080;
+  int width = 1920;
+
+  dai::Pipeline pipeline;
+
+  // Define source and output
+  auto camRGB = pipeline.create<dai::node::ColorCamera>();
+  auto xout = pipeline.create<dai::node::XLinkOut>();
+
+  xout->setStreamName("rgb");
+  xout->input.setBlocking(false);
+  xout->input.setQueueSize(1);
+
+  // Properties
+  camRGB->setResolution(
+      dai::ColorCameraProperties::SensorResolution::THE_1080_P);
+  camRGB->setFps(60);
+  camRGB->setVideoSize({width, height});
+
+  // Linking
+  camRGB->video.link(xout->input);
+
+  dai::Device device(pipeline);
+  auto qRGB = device.getOutputQueue("rgb", 4, false);
+
+  while (true) {
+    auto inRGB = qRGB->get<dai::ImgFrame>();
+
+    cv::imshow("rgb", inRGB->getCvFrame());
+
+    int key = cv::waitKey(1);
+    if (key == 'q' || key == 'Q')
+      return 0;
+  }
+
+  return 0;
 }
